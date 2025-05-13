2401f15d36926eef4d17cb2491b35aa5eb2ab0a0
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-13 05:36:55 +0500

    Add URL patterns for home, cars, about, login, logout, and car management views

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    ]
+    path('', views.home, name='home'),
+    path('cars/', views.cars, name='cars'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add_car/', views.add_car, name='add_car'),
+    path('rent_car/<int:car_id>/', views.rent_car, name='rent_car'),
+    path('return_car/<int:car_id>/', views.return_car, name='return_car'),
+    path('delete_car/<int:car_id>/', views.delete_car, name='delete_car'),
+]
